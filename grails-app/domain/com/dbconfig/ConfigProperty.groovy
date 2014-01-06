a0d5c96374c58e7e3a4000c645d73c2989328ff2
guanyu guo <ggy1986@163.com>
2014-01-06 15:52:30 +1100

    ConfigurationHolder insteaded GrailsApplication

--- grails-app/domain/com/dbconfig/ConfigProperty.groovy
+++ grails-app/domain/com/dbconfig/ConfigProperty.groovy
@@ -1,6 +1,6 @@
 package com.dbconfig
 
-import org.codehaus.groovy.grails.commons.GrailsApplication as CH
+import org.codehaus.groovy.grails.commons.ConfigurationHolder as CH
 import org.codehaus.groovy.runtime.DefaultGroovyMethods
 
 class ConfigProperty {
