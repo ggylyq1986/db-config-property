d5004e7fe6c32b979a0802b8345257c9d2a4c44a
guanyu <guanyu@UOW-J3WRF2S.ad.uow.edu.au>
2014-01-07 15:05:45 +1100

    Changed the description of the plugin

--- DbConfigPropertyGrailsPlugin.groovy
+++ DbConfigPropertyGrailsPlugin.groovy
@@ -12,10 +12,10 @@ class DbConfigPropertyGrailsPlugin {
 
     // TODO Fill in these fields
     def title = "Db Config Property Plugin - Special Team UOW's ITS" // Headline display name of the plugin
-    def author = "Peter, Guanyu"
-    def authorEmail = ""
+    def author = "Peter Newnam, Guanyu Guo"
+    def authorEmail = "Guanyu@uow.edu.au"
     def description = '''\
-Brief summary/description of the plugin.
+This plugin gives your application the ability to change the config properties without restarting the application. The values in Config.groovy are persisted in database when application is run-for the first time after installing the plugin. But if you do not set up dbCreate in your project’s DataSource.groovy, you need to create config_property table manually (create table config_property (id number(19,0) not null, version number(19,0) not null, description varchar2(100 char), key varchar2(100 char) not null, value varchar2(255 char) not null, primary key (id));) 
 '''
 
     // URL to the plugin's documentation
